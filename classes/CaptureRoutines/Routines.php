7639a43c6e2bf34ed6220b0a560c686fcf3d0d80
mjansen <mjansen@databay.de>
2023-05-19 19:46:03 +0200

    Add ILIAS 8 compatibility

--- classes/CaptureRoutines/Routines.php
+++ classes/CaptureRoutines/Routines.php
@@ -1,68 +1,61 @@
 <?php
 
+/**
+ * This file is part of ILIAS, a powerful learning management system
+ * published by ILIAS open source e-Learning e.V.
+ *
+ * ILIAS is licensed with the GPL-3.0,
+ * see https://www.gnu.org/licenses/gpl-3.0.en.html
+ * You should have received a copy of said license along with the
+ * source code, too.
+ *
+ * If this is not the case or you just want to try ILIAS, you'll find
+ * us at:
+ * https://www.ilias.de
+ * https://github.com/ILIAS-eLearning
+ *
+ *********************************************************************/
+
 namespace QU\PowerBiReportingProvider\CaptureRoutines;
 
 use QU\LERQ\API\DataCaptureRoutinesInterface;
 use QU\LERQ\Model\EventModel;
 
-/**
- * Class Routines
- * @author Ingmar Szmais <iszmais@databay.de>
- * @since 25.04.19
- */
 class Routines implements DataCaptureRoutinesInterface
 {
-	/**
-	 * @inheritDoc
-	 */
-	public function getOverrides(): array
-	{
-		return [
-			'collectUserData' => false,
-			'collectUDFData' => false,
-			'collectMemberData' => false,
-			'collectLpPeriod' => false,
-			'collectObjectData' => false,
-		];
-	}
+    public function getOverrides(): array
+    {
+        return [
+            'collectUserData' => false,
+            'collectUDFData' => false,
+            'collectMemberData' => false,
+            'collectLpPeriod' => false,
+            'collectObjectData' => false,
+        ];
+    }
 
-	/**
-	 * @inheritDoc
-	 */
-	public function collectLpPeriod(EventModel $event): array
-	{
-		return [];
-	}
+    public function collectLpPeriod(EventModel $event): array
+    {
+        return [];
+    }
 
-	/**
-	 * @inheritDoc
-	 */
-	public function collectUDFData(EventModel $event): array
-	{
-		return [];
-	}
+    public function collectUDFData(EventModel $event): array
+    {
+        return [];
+    }
 
-	/**
-	 * @inheritDoc
-	 */
-	public function collectUserData(EventModel $event): array
-	{
-		return [];
-	}
+    public function collectUserData(EventModel $event): array
+    {
+        return [];
+    }
 
-	/**
-	 * @inheritDoc
-	 */
-	public function collectMemberData(EventModel $event): array
-	{
-		return [];
-	}
+    public function collectMemberData(EventModel $event): array
+    {
+        return [];
+    }
 
-	/**
-	 * @inheritDoc
-	 */
-	public function collectObjectData(EventModel $event): array
-	{
-		return [];
-	}
+    public function collectObjectData(EventModel $event): array
+    {
+        return [];
+    }
 }
